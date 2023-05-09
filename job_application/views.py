2ae5eab5348fd9951ba8e045c6d87fe0b5b45567
ade-t <ad_elyn@hotmail.com>
2023-05-10 01:30:59 +0800

    application form

--- job_application/views.py
+++ job_application/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
-# Create your views here.
+
+# import application form class
+# extract user values from form using cleaned data dictionary
+def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            messages.success(request, "Form submitted successfully")
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
